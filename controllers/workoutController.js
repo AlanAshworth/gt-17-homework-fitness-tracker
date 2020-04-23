3acdef3a50f076c1e2cc42699dba5af575b9a3b9
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 18:20:36 -0400

    Add restful route for creating workouts.

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -2,24 +2,37 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req,res) => {
-    db.Workout.find({})
-    .then(workouts => {
-        console.log(workouts);
-        res.json(workouts);
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((workouts) => {
+      console.log(workouts);
+      res.json(workouts);
     })
-    .catch( err => {
-        console.log(err);
-        res.json(err);
-    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-router.get("/api/workouts/range", (req,res) => {
-    db.Workout.find({}).then(workouts => {
-        res.json(workouts);
-    }).catch (err => {
-        res.json(err);
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((workouts) => {
+      res.json(workouts);
     })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      res.status(500);
+      res.json(err);
+    });
 });
 
 module.exports = router;
