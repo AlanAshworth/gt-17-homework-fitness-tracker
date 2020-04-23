d3363b45dcba147bc010dde0821086aab740e60b
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 17:36:03 -0400

    Add basic Schema 'meat'.

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,6 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+    {
+        type: String,
+        trim: true,
+        required: "Select workout type."
+    },
+    {
+        name: String,
+        trim: true,
+        required: "Enter your name."
+    },
+    {
+        duration: Number,
+        required: "Enter number of minutes to complete this exercise."
+    },
+    {
+        weight: Number
+    },
+    {
+        reps: Number
+    },
+    {
+        sets: Number
+    },
+    {
+        distance: Number
+    }
+    ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
