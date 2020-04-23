5c0e1d79add43151f1eb790f3c8f464401bbc97d
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 16:58:09 -0400

    Add Mongoose data connection.

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    { useNewUrlParser: true }
+  );
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
