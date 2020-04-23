837419028ed3271e85db917981c627881f25c064
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 17:49:21 -0400

    Format styling

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,38 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
     {
-        type: String,
-        trim: true,
-        required: "Select workout type."
-    },
-    {
-        name: String,
-        trim: true,
-        required: "Enter your name."
+      type: String,
+      trim: true,
+      required: "Select workout type.",
     },
     {
-        duration: Number,
-        required: "Enter number of minutes to complete this exercise."
+      name: String,
+      trim: true,
+      required: "Enter your name.",
     },
     {
-        weight: Number
+      duration: Number,
+      required: "Enter number of minutes to complete this exercise.",
     },
     {
-        reps: Number
+      weight: Number,
     },
     {
-        sets: Number
+      reps: Number,
     },
     {
-        distance: Number
-    }
-    ]
+      sets: Number,
+    },
+    {
+      distance: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
