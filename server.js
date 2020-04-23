52a70c71fea1de015666c2a89d9f6a0ec39efd3a
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 17:58:22 -0400

    Update server with workoutController to handle routes.

--- server.js
+++ server.js
@@ -7,19 +7,25 @@ const app = express();
 
 const Workout = require("./models/workoutModel.js");
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-    { useNewUrlParser: true }
-  );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
 
-  app.use(morgan('dev', {
-    skip: function (req, res) { return res.statusCode < 400 }
-  }))
+app.use(
+  morgan("dev", {
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+  })
+);
+
+const workoutRoutes = require("./controllers/workoutController.js");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(workoutRoutes);
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
