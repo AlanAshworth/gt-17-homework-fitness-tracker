0ec9cbef6424a3db215254ea81288779182fbbfe
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 18:25:24 -0400

    Add update route for workouts.  Also edit reponses to more sensible names to fall inline with restful route scheme.

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -4,9 +4,9 @@ const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then((workouts) => {
-      console.log(workouts);
-      res.json(workouts);
+    .then((getWorkouts) => {
+      console.log(getWorkouts);
+      res.json(getWorkouts);
     })
     .catch((err) => {
       console.log(err);
@@ -16,8 +16,8 @@ router.get("/api/workouts", (req, res) => {
 
 router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
-    .then((workouts) => {
-      res.json(workouts);
+    .then((getWorkoutsRange) => {
+      res.json(getWorkoutsRange);
     })
     .catch((err) => {
       res.json(err);
@@ -26,11 +26,21 @@ router.get("/api/workouts/range", (req, res) => {
 
 router.post("/api/workouts", (req, res) => {
   db.Workout.create(req.body)
-    .then((newWorkout) => {
-      res.json(newWorkout);
+    .then((postNewWorkout) => {
+      res.json(postNewWorkout);
     })
     .catch((err) => {
       res.status(500);
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate({ _id: req.params.id })
+    .then((updateWorkout) => {
+      res.json(updateWorkout);
+    })
+    .catch((err) => {
       res.json(err);
     });
 });
