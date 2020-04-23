78745950ed6d1e523b959e7bbbaeba0e00a5fe57
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 18:07:25 -0400

    Develop restful route for read all.

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
+router.get("/api/workouts", (req,res) => {
+    db.Workout.find({})
+    .then(workouts => {
+        console.log(workouts);
+        res.json(workouts);
+    })
+    .catch( err => {
+        console.log(err);
+        res.json(err);
+    })
+});
+
 module.exports = router;
