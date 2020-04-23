796e2b1b84f46a12405d2ef8d3fbd6718a596e92
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 18:49:28 -0400

    Update server with routing controller for html files.

--- server.js
+++ server.js
@@ -20,11 +20,13 @@ app.use(
 );
 
 const workoutRoutes = require("./controllers/workoutController.js");
+const htmlRoutes = require("./controllers/htmlController.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(workoutRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
