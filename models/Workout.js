0f8e051bd8f87532b7e6cd808a3773ed94500d7d
Alan Ashworth <awashworth927@gmail.com>
2020-04-23 22:58:28 -0400

    Remove virtual. Not working.

--- models/Workout.js
+++ models/Workout.js
@@ -1,52 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
-  {
-    day: {
-      type: Date,
-      default: Date.now(),
-    },
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-        },
-        name: {
-          type: String,
-          trim: true,
-        },
-        duration: {
-          type: Number,
-        },
-        weight: {
-          type: Number,
-        },
-        reps: {
-          type: Number,
-        },
-        sets: {
-          type: Number,
-        },
-        distance: {
-          type: Number,
-        },
-      },
-    ],
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
   },
-  {
-    toJSON: {
-      virtuals: true,
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
-  }
-);
-
-WorkoutSchema.virtual("totalDuration").get(function () {
-  const sum = this.exercises.reduce((accum, curr) => {
-    return accum.duration + curr.duration;
-  });
-  return sum.duration;
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
